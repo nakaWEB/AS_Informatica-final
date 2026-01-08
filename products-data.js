dfb6e4b474c97dc259690d235482c19469c52f8c
nakaWEB <166887497+nakaWEB@users.noreply.github.com>
2026-01-08 16:48:03 -0300

    Add files via upload

---
+++ products-data.js
@@ -0,0 +1,62 @@
+const PRODUTOS = [
+  {
+    id: "lapis-cor",
+    nome: "Lápis de Cor Faber",
+    preco: 28.99,
+    imagem: "../lapis faber.webp",
+    descricao: "Kit com 24 cores vibrantes, ideal para arte e escola",
+    tags: ["material escolar", "desenho", "arte", "lapis", "cores", "escolar"],
+    categoria: "papelaria",
+    whatsapp: "https://wa.me/55289992788?text=Olá,%20gostaria%20de%20saber%20o%20Lapis%20de%20cor%20da%20Faber%20castelo"
+  },
+  {
+    id: "caderno-tiibra-10-materias",
+    nome: "Tilibra Universitário 10 Matérias",
+    preco: 17.28,
+    imagem: "../caderno tilibra.jpg",
+    descricao: "Capa dura, 200 folhas pautadas",
+    tags: ["material escolar", "caderno", "universitario", "estudo", "papelaria"],
+    categoria: "papelaria",
+    whatsapp: "https://wa.me/55289992788?text=Olá,%20gostaria%20de%20saber%20sobre%20Caneta%20Esferográfica"
+  },
+  {
+    id: "caneta-esferografica",
+    nome: "Caneta Bic Azul Esferográfica Cristal 50 Unidades 1.0 Mm",
+    preco: 25.90,
+    imagem: "Caneta Bic Azul Esferográfica Cristal 50 Unidades 1.0 Mm.webp",
+    descricao: "Kit com 10 unidades, tinta azul",
+    tags: ["material escolar", "caneta", "escrita", "escolar", "papelaria"],
+    categoria: "papelaria",
+    whatsapp: "https://wa.me/55289992788?text=Olá,%20gostaria%20de%20saber%20sobre%20Caneta%20Esferográfica"
+  },
+    {
+    id: "cardeneta",
+    nome: "Caderneta A6 flexível Smile Preta com 80 folhas pautadas",
+    preco: 5.50,
+    imagem: "../Caderneta A6 flexível Smile Preta com 80 folhas pautadas.jpg",
+    descricao: "Caderno de anotação com molas",
+    tags: ["material escolar", "escrita", "escolar", "papelaria"],
+    categoria: "papelaria",
+    whatsapp: "https://wa.me/55289992788?text=Olá,%20gostaria%20de%20saber%20sobre%20Caneta%20Esferográfica"
+  },
+    {
+    id: "A-520M",
+    nome: "Placa Mãe A520M K V2 AMD AM4 MATX DDR4 Gigabyte Cor Preto",
+    preco: 667.90,
+    imagem: "../PlacaMãeA520MKV2AMDAM4MATXDDR4GigabyteCorPreto.webp",
+    descricao: "Caderno de anotação com molas",
+    tags: ["Placa mão", "PC", "Computador", "informatica"],
+    categoria: "informatica",
+    whatsapp: "https://wa.me/55289992788?text=Olá,%20gostaria%20de%20saber%20sobre%20Caneta%20Esferográfica"
+  },
+    {
+    id: "Processador-Gamer-Intel-Core-i7-4790",
+    nome: "Processador Gamer Intel Core i7-4790 4 Núcleos 4GHz com Gráfica Integrada HD 4600",
+    preco: 355.00,
+    imagem: "../Core i7-4790 4 Núcleos.webp",
+    descricao: "Caderno de anotação com molas",
+    tags: ["PC", "computador", "processador", "intel"],
+    categoria: "Informatica",
+    whatsapp: "https://wa.me/55289992788?text=Olá,%20gostaria%20de%20saber%20sobre%20Caneta%20Esferográfica"
+  }
+];
